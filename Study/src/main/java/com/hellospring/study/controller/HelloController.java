7283ce66e3e5009e362f8f90695828ce18f7a4f8
jeongkihun <jys777999@gmail.com>
2023-11-22 23:24:29 +0900

    11-22

--- Study/src/main/java/com/hellospring/study/controller/HelloController.java
+++ Study/src/main/java/com/hellospring/study/controller/HelloController.java
@@ -4,6 +4,7 @@ package com.hellospring.study.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -13,5 +14,11 @@ public class HelloController {
         model.addAttribute("data","hello!!");
         return "hello";
 
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
     }
 }
