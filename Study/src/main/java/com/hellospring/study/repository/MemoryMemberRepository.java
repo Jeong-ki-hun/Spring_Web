d5cc084acaf35c92626bd0a3c782c5bdd1b59f41
jeongkihun <jys777999@gmail.com>
2023-12-07 22:19:53 +0900

    12-07

--- Study/src/main/java/com/hellospring/study/repository/MemoryMemberRepository.java
+++ Study/src/main/java/com/hellospring/study/repository/MemoryMemberRepository.java
@@ -4,9 +4,11 @@ import com.hellospring.study.domain.Member;
 
 import java.util.*;
 
-public class MemoryMemberRepository implements MemberRepository{
+public class MemoryMemberRepository implements MemberRepository {
 
-    private static Map<Long, Member> store  = new HashMap<>();
+
+
+    private static Map<Long, Member> store = new HashMap<>();
     // 위와같은 경우가 실무일 경우에는 동시성문제때문에 Concurrent라는 변수를 사용해야한다
     private static long sequence = 0L;
 
@@ -19,18 +21,25 @@ public class MemoryMemberRepository implements MemberRepository{
 
     @Override
     public Optional<Member> findById(Long id) {
-        return Optional.ofNullable(store.get(id));
-    }
 
-    @Override
-    public Optional<Member> findByname(String name) {
-        store.values().stream()
-                .filter(member -> member.getName().equals(name))
-                .findAny();
+        return Optional.ofNullable(store.get(id));
     }
 
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
+
+    @Override
+    public Optional<Member> findByname(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
 }
+
+
